d6084bb206b7d370d48b6348513845d63a078f68
Andrei Simionescu <andreisimi02@gmail.com>
2026-03-09 12:05:35 +0100

    larger batch size validation

--- downstream_phase/run_phase_training.py
+++ downstream_phase/run_phase_training.py
@@ -529,7 +529,7 @@ def main(args, ds_init):
         data_loader_val = torch.utils.data.DataLoader(
             dataset_val,
             sampler=sampler_val,
-            batch_size=int(4 * args.batch_size),
+            batch_size=int(8 * args.batch_size),
             num_workers=args.num_workers,
             pin_memory=args.pin_mem,
             drop_last=False,
